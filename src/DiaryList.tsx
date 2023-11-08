7d3db15a02b56da8f534a2cabea8abc280acddcf
origin1508 <origin1508@gmail.com>
2023-11-08 22:42:48 +0900

    fix: import DiaryItem

--- src/DiaryList.tsx
+++ src/DiaryList.tsx
@@ -1,4 +1,6 @@
+import DiaryItem from "./DiaryItem";
 import { DiaryListProps } from "./types/diary";
+
 const DiaryList = ({ diaryList }: DiaryListProps) => {
   if (!diaryList) return <div>작성한 일기가 없습니다.</div>;
   return (
@@ -9,14 +11,7 @@ const DiaryList = ({ diaryList }: DiaryListProps) => {
       </div>
       <div>
         {diaryList.map((item) => {
-          return (
-            <div key={item.id}>
-              <div>작성자 : {item.author}</div>
-              <div>일기 : {item.content}</div>
-              <div>감정 : {item.emotion}</div>
-              <div>작성시간(ms) : {item.created_date}</div>
-            </div>
-          );
+          return <DiaryItem key={item.id} {...item} />;
         })}
       </div>
     </div>
