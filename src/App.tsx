57431741abbb397e13f657e74a915ab1ebcefd69
origin1508 <origin1508@gmail.com>
2023-11-08 22:42:20 +0900

    fix: dummyList type에 맞춰 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,28 @@ const dummyList = [
     author: "홍길동",
     content: "일기 1일차",
     emotion: 5,
-    created_date: new Date().getTime(),
+    createdDate: new Date().getTime(),
   },
   {
     id: 2,
     author: "둘리",
     content: "일기 2일차",
     emotion: 3,
-    created_date: new Date().getTime(),
+    createdDate: new Date().getTime(),
   },
   {
     id: 3,
     author: "철수",
     content: "일기 3일차",
     emotion: 2,
-    created_date: new Date().getTime(),
+    createdDate: new Date().getTime(),
   },
   {
     id: 4,
     author: "영희",
     content: "일기 4일차",
     emotion: 1,
-    created_date: new Date().getTime(),
+    createdDate: new Date().getTime(),
   },
 ];
 
