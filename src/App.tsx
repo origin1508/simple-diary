8047a52a0808cf3df2cf76044a7f984db6d0865e
origin1508 <origin1508@gmail.com>
2023-11-10 11:03:17 +0900

    fix: onDelete를 이용해 일기 삭제 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import "./App.css";
-import { Data, OnCreate } from "./types/diary";
+import { Data, OnCreate, OnDelete } from "./types/diary";
 
 function App() {
   const [data, setData] = useState<Data[]>([]);
@@ -24,10 +24,20 @@ function App() {
       return [newItem, ...prev];
     });
   };
+
+  const onDelete: OnDelete = (targetId) => {
+    if (window.confirm(`${targetId}번째 일기를 정말 삭제하시겠습니까?`)) {
+      const newDiaryList = data.filter((item) => item.id !== targetId);
+      setData(newDiaryList);
+    }
+
+    return;
+  };
+
   return (
     <div>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList diaryList={data} onDelete={onDelete} />
     </div>
   );
 }
