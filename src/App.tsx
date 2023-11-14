eda49616de6b61d1d3593781595a080e19a8d545
origin1508 <origin1508@gmail.com>
2023-11-14 21:51:54 +0900

    fix: jsonplaceholder를 이용해 api 호출 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import "./App.css";
 import { Data, OnCreate, OnRemove, OnEdit } from "./types/diary";
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState<Data[]>([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res: {
+      body: string;
+      email: string;
+      id: number;
+      name: string;
+      postId: number;
+    }[] = await fetch("https://jsonplaceholder.typicode.com/comments", {
+      method: "get",
+    }).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        createdDate: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate: OnCreate = (author, content, emotion) => {
     const createdDate = new Date().getTime();
